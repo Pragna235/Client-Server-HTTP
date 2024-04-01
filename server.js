a39eb4f5eab8ca375bcc86375372c45d29d31802
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-04-01 07:24:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http'); 
 const hostname = '127.0.0.1'; 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const requestListener = function(req,res){ 
 res.statusCode = 200; res.setHeader('ContentType','text/plain');
 if(req.method==='POST'){
